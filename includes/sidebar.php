b568dd1924b6a28b8b020c8ebece570229a2ab1e
Mhezel Mohammad <mhezelkhan@gmail.com>
2022-11-18 17:05:02 +0800

    update search bar module and create new search.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,21 +1,9 @@
 <!-- Blog Sidebar Widgets Column -->
 <div class="col-md-4">
-    <?php
-        if(isset($_POST['submit'])){
-            $search = $_POST['search'];
-           
-            $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search' ";
-            $search_query = mysqli_query($conn,$query);
-
-            if(!$search_query){
-                die("Connection Failure" . mysqli_error($conn));
-            }
-        }
-        ?>
                  <!-- Blog Search Well -->
                  <div class="well">
                      <h4>Blog Search</h4>
-                     <form action="" method="post">
+                     <form action="search.php" method="post">
                      <div class="input-group">
                          <input name="search" type="text" class="form-control">
                          <span class="input-group-btn">
---
+++ search.php
@@ -0,0 +1,82 @@
+
+<?php include "includes/header.php"; ?>
+
+     <!-- Navigation -->
+     <?php include "includes/navigation.php"; ?> 
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+            <?php
+
+            if(isset($_POST['submit'])){
+                $search = $_POST['search'];
+               
+                $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search' ";
+                $search_query = mysqli_query($conn,$query);
+
+                if(!$search_query){
+
+                    die("Unable to process data" . mysqli_error($conn));
+                }
+
+                $count = mysqli_num_rows($search_query);
+
+                if($count == 0){
+
+                    die("Unable to process data");
+
+                }else{
+
+                 $query = "SELECT * FROM posts";
+                 $select_all_post_query = mysqli_query($conn, $query);
+
+                while($row = mysqli_fetch_assoc($select_all_post_query)){
+                 
+                  $post_title = $row['post_title'];
+                  $post_author = $row['post_author'];
+                  $post_date = $row['post_date'];
+                  $post_image = $row['post_image'];
+                  $post_content = $row['post_content'];
+             ?>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="#"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></a></p>
+                <hr>
+                <img class="img-responsive" src="img/<?php echo $post_image; ?>" alt="">
+                <hr>
+                <p><?php echo $post_content ?></a></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <hr>
+
+            <?php } 
+                }
+            }
+            ?>
+            </div>
+              
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include "includes/sidebar.php"; ?>
+
+        </div>
+        <!-- /.row -->
+        <hr>
+    </div>
+
+<?php include "includes/footer.php"; ?>
+
+
